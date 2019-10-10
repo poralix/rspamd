cc7824c3da9aead3a66e1c3bc7dcf8bbfe3e85bc
Alex <alexey@nsk21.ru>
2019-10-10 11:55:46 +0700

    Correct permissions on install/update

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 #
 #   Rspamd web interface plugin for Directadmin $ 0.1.2
 #   ==============================================================================
-#          Last modified: Tue May 14 12:30:43 +07 2019
+#          Last modified: Thu Oct 10 11:54:54 +07 2019
 #   ==============================================================================
 #         Written by Alex S Grebenschikov (support@poralix.com)
 #         Copyright 2019 by Alex S Grebenschikov (support@poralix.com)
@@ -14,10 +14,14 @@
 DIR="/usr/local/directadmin/plugins/rspamd";
 
 chown -R diradmin:diradmin "${DIR}";
-chmod 700 "${DIR}/admin/"*.raw;
-chmod 700 "${DIR}/admin/"*.html;
-chmod 700 "${DIR}/data/";
-chmod 700 "${DIR}/exec/";
+chmod 711 "${DIR}";
+chmod 755 "${DIR}/hooks/";
+chmod 644 "${DIR}/hooks/admin_img.html";
+chmod 644 "${DIR}/hooks/admin_txt.html";
+chmod 710 "${DIR}/admin/"*.raw;
+chmod 710 "${DIR}/admin/"*.html;
+chmod 710 "${DIR}/data/";
+chmod 710 "${DIR}/exec/";
 
 perl -pi -e "s/^active=no/active=yes/" "${DIR}/plugin.conf";
 perl -pi -e "s/^installed=no/installed=yes/" "${DIR}/plugin.conf";
