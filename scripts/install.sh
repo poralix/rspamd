5e54cf510888a9a79475cbe6fe9f3647abd5dfe6
Alex <alexey@nsk21.ru>
2020-05-21 23:01:06 +0700

    Minor fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,20 +13,33 @@
 
 DIR="/usr/local/directadmin/plugins/rspamd";
 
-chown -R _rspamd:_rspamd "${DIR}";
-chmod 700 "${DIR}/admin/"*.raw;
-chmod 700 "${DIR}/admin/"*.html;
-chmod 700 "${DIR}/data/";
-chmod 700 "${DIR}/exec/";
+chown -R diradmin:diradmin "${DIR}";
+id _rspamd >/dev/null 2>&1; RVAL=$?;
 
-perl -pi -e "s/^active=no/active=yes/" "${DIR}/plugin.conf";
-perl -pi -e "s/^installed=no/installed=yes/" "${DIR}/plugin.conf";
-perl -pi -e "s/.*'RSPAMD_SOCKET'.*//" "${DIR}/exec/settings.inc.php";
+if [ "${RVAL}" == "0" ]; then
+{
+    chown -R _rspamd:_rspamd "${DIR}";
+    chmod 700 "${DIR}/admin/"*.raw;
+    chmod 700 "${DIR}/admin/"*.html;
+    chmod 700 "${DIR}/data/";
+    chmod 700 "${DIR}/exec/";
 
-if [ -S "/var/run/rspamd/rspamd_controller.sock" ]; then
-    echo "if (!defined('RSPAMD_SOCKET')) define('RSPAMD_SOCKET','/var/run/rspamd/rspamd_controller.sock');" >> "${DIR}/exec/settings.inc.php";
+    perl -pi -e "s/^active=no/active=yes/" "${DIR}/plugin.conf";
+    perl -pi -e "s/^installed=no/installed=yes/" "${DIR}/plugin.conf";
+    perl -pi -e "s/.*'RSPAMD_SOCKET'.*//" "${DIR}/exec/settings.inc.php";
+
+    if [ -S "/var/run/rspamd/rspamd_controller.sock" ]; then
+        echo "if (!defined('RSPAMD_SOCKET')) define('RSPAMD_SOCKET','/var/run/rspamd/rspamd_controller.sock');" >> "${DIR}/exec/settings.inc.php";
+    fi;
+
+    echo "Plugin installed";
+}
+else
+{
+    perl -pi -e "s/^active=.*/active=no/" "${DIR}/plugin.conf";
+    perl -pi -e "s/^installed=.*/installed=no/" "${DIR}/plugin.conf";
+    echo "No Rspamd is found to be installed!<br>First install Rspamd and then re-install the plugin!<br>";
+}
 fi;
-
-echo "Plugin installed";
 
 exit 0;
